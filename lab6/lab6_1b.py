b45e7c79fc85496d890e92ee610d6d574bf2c410
Ruigetsu <dynovskianton@gmail.com>
2025-10-11 22:08:16 +0300

    lab5_protect and lab6 creation of main

--- lab6/lab6_1b.py
+++ lab6/lab6_1b.py
@@ -1,9 +1,8 @@
 list_of_num = list(map(int,input("Введите элементы списка через пробел: ").split()))
 num = int(input("Введите элемент для вставки: "))
 indx = int(input("Введите индекс вставки элемента: "))
-
-list_of_num.append("")
 if indx == -1 or indx == len(list_of_num)-1:
+    list_of_num.append("")
     list_of_num[indx] = num
     print(f"Новый список = {list_of_num}")
 elif indx > len(list_of_num) or indx < -len(list_of_num) - 1:
