40a7f2af9e1926874dadfb50a5fbcdf1f4cc5f8e
Ruigetsu <dynovskianton@gmail.com>
2025-10-15 14:34:42 +0300

    change in main.py

--- lab6/main.py
+++ lab6/main.py
@@ -9,34 +9,34 @@ print("Введите номер задания или 0 для выхода:\n\
       Для завершения введите: 0\n")
 
 while True: 
-      num = input("Введите номер задания: ")
-      match num:
+      exercise = input("Введите номер задания: ")
+      match exercise:
             case "1a":
-                  f = open("lab6_1a.py", encoding='utf-8').read()
+                  f = open("lab6_1a.py").read()
                   exec(f)
                   print("\n")
             case "1b":
-                  f = open("lab6_1b.py", encoding='utf-8').read()
+                  f = open("lab6_1b.py").read()
                   exec(f)
                   print("\n")
             case "2a":
-                  f = open("lab6_2a.py", encoding='utf-8').read()
+                  f = open("lab6_2a.py").read()
                   exec(f)
                   print("\n")
             case "2b":
-                  f = open("lab6_2b.py", encoding='utf-8').read()
+                  f = open("lab6_2b.py").read()
                   exec(f)
                   print("\n")
             case "3":
-                  f = open("lab6_3.py", encoding='utf-8').read()
+                  f = open("lab6_3.py").read()
                   exec(f)
                   print("\n")
             case "4":
-                  f = open("lab6_4_7.py", encoding='utf-8').read()
+                  f = open("lab6_4_7.py").read()
                   exec(f)
                   print("\n")
             case "5":
-                  f = open("lab6_5_2.py", encoding='utf-8').read()
+                  f = open("lab6_5_2.py").read()
                   exec(f)
                   print("\n")
             case "0":
