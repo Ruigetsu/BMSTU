730db75196854afe9b115a77740af7a3e4e7b67a
Ruigetsu <dynovskianton@gmail.com>
2025-10-15 21:50:27 +0300

    lab7

---
+++ lab7/lab7_1_2.py
@@ -0,0 +1,11 @@
+list_of_num = list(map(int,input("Введите числа через пробел:").split()))
+flag_to_move_left = 0
+
+for i in range(len(list_of_num)):
+    if list_of_num[i] % 2 == 0:
+        list_of_num[i] = None
+        flag_to_move_left += 1
+    else: 
+        list_of_num[i-flag_to_move_left] = list_of_num[i]
+list_of_num = list_of_num[:-flag_to_move_left]
+print(list_of_num)
---
+++ lab7/main.py
@@ -0,0 +1,32 @@
+#1223345
+print("Введите номер задания или 0 для выхода:\n\
+      Для задания 1 введите : 1\n\
+      Для задания 2 введите : 2\n\
+      Для задания 3 введите : 3\n\
+      Для задания 4 введите : 4\n\
+      Для завершения введите: 0\n")
+
+while True: 
+      exercise = input("Введите номер задания: ")
+      match exercise:
+            case "1":
+                  f = open("lab7_1_2.py").read()
+                  exec(f)
+                  print("\n")
+            case "2":
+                  f = open("lab7_2_2.py").read()
+                  exec(f)
+                  print("\n")
+            case "3":
+                  f = open("lab7_3_4.py").read()
+                  exec(f)
+                  print("\n")
+            case "4":
+                  f = open("lab7_4_3.py").read()
+                  exec(f)
+                  print("\n")
+            case "0":
+                  print("Завершение работы")
+                  break
+            case _:
+                  print("Вы ввели неправильное число")
