0df81e6ae8c6f3139a7eb42a705b93ebcc4402bb
Ruigetsu <dynovskianton@gmail.com>
2025-10-15 14:30:09 +0300

    moving on a new branch for macos

--- lab6/lab6_1b.py
+++ lab6/lab6_1b.py
@@ -5,11 +5,11 @@ indx = int(input("Введите индекс вставки элемента: "
 if indx > len(list_of_num) or indx < -len(list_of_num) - 1:
     print("Вы ввели неверный индекс")
 elif indx == -1 or indx == len(list_of_num)-1:
-    list_of_num.append("")
+    list_of_num.append(None)
     list_of_num[indx] = num
     print(f"Новый список = {list_of_num}")
 else:
-    list_of_num.append("")
+    list_of_num.append(None)
     for i in range(len(list_of_num)-1,indx-1,-1): 
         list_of_num[i] = list_of_num[i-1]
         if i == indx: 
--- lab6/lab6_2b.py
+++ lab6/lab6_2b.py
@@ -6,8 +6,8 @@ if indx > len(list_of_num) or indx < -len(list_of_num) - 1:
 else:
     for i in range(indx,len(list_of_num)-1): 
         if i == len(list_of_num) - 1:
-            list_of_num[i] == ""
+            list_of_num[i] == None
         else: 
             list_of_num[i] = list_of_num[i+1]
-list_of_num = list_of_num[:-1]
+list_of_num.pop()
 print(f"Новый список = {list_of_num}")
